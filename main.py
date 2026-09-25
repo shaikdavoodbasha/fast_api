14ff3212d6aa69272d4f6223bdbfec036caf9d90
shaikdavoodbasha <shannushaik080@gmail.com>
2026-09-25 22:04:51 +0530

    one-seven

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def read_root():
 
 @app.get("/greet")
 def greet():
-    return {'Message':"Hello bro Good"}
+    return {'Message':"Hello bro Gooddoekdjdl"}
 
 @app.get("/greet/")
 def greet_name(name:str,age:Optional[int] =None):
